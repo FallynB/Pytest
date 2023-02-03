7534f1cc069bff5191bb74e5a5850d4466d834d8
Asia <Asiasm7@gmail.com>
2023-02-03 10:56:19 -0800

    Fixed error

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,6 @@ def evens(n):
     Returns a list of even numbers from 0 to n inclusive.
     '''
 
-    evenlist = range(n+1)
+    evenlist = range(n + 1)
     evenlist = filter(lambda m: m % 2 == 0, evenlist)
     return list(evenlist)
